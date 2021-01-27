6dfe53d1a7afd3ab76109e67027babf93d17d81c
snaphuman <fhernandezn@gmail.com>
2021-01-27 13:39:35 -0500

    bind customer data to children and apply custom pipes

--- src/app/customers.module.ts
+++ src/app/customers.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from './shared/shared.module';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomersListComponent } from './customers/customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers/customers-list/filter-textbox.component';
 
+
 @NgModule({
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
   declarations: [
     CustomersComponent,
     CustomersListComponent,
     FilterTextboxComponent
-  ],
-  imports: [
-    CommonModule
   ],
   exports: [ CustomersComponent ]
 })
--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from '../../shared/interfaces';
 
@@ -7,17 +7,40 @@ import { ICustomer } from '../../shared/interfaces';
     templateUrl: './customers-list.component.html'
 })
 export class CustomersListComponent implements OnInit {
+    private _customers: ICustomer[];
+    @Input() get customers(): ICustomer[] {
+        return this._customers;
+    }
+
+    set customers(value: ICustomer[]) {
+        if(value) {
+            this.filteredCustomers = this._customers = value;
+            this.calculateOrders();
+        }
+    }
 
     filteredCustomers: ICustomer[];
     customersOrderTotal: number;
     currencyCode: string;
 
     constructor() {
+        this._customers = [];
         this.filteredCustomers = [];
         this.customersOrderTotal = 0;
         this.currencyCode = 'USD'
     }
     
     ngOnInit() {
+
+    }
+
+    calculateOrders() {
+        this.filteredCustomers.forEach((customer: ICustomer) => {
+            this.customersOrderTotal += customer.orderTotal;
+        });
+    }
+
+    sort(prop: string) {
+       
     }
 }
