3aed664390a87a8b8560265f5d0d3057bb1852a0
Fabian Hernandez Nieto <fhernandezn@gmail.com>
2021-02-19 10:13:55 -0500

    update angular cdk and remove unused import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { CapitalizePipe } from './capitalize.pipe';
-
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-
-
 @NgModule({
-    imports: [ CollapseModule.forRoot() ],
     declarations: [ CapitalizePipe ],
     exports: [ CapitalizePipe ],
 })
